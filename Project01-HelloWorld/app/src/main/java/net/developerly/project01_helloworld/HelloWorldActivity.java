c8effa32ad92aa6172b77d280c4c99e9716160c9
LY <coderyliu@gmail.com>
2017-03-29 16:45:08 +0800

    Add Project01-HelloWorld

---
+++ Project01-HelloWorld/app/src/main/java/net/developerly/project01_helloworld/HelloWorldActivity.java
@@ -0,0 +1,17 @@
+package net.developerly.project01_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    private  static final String TAG = "HelloWorldActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.d(TAG, "onCreate: Test DeBug");
+    }
+}
