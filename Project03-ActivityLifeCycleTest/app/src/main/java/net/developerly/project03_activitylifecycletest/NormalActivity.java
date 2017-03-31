57a881757899ec308d76a93e143c2f1ab3f56ca9
LY <coderyliu@gmail.com>
2017-03-31 16:46:01 +0800

    Add Project03-ActivityLifeCycleTest

---
+++ Project03-ActivityLifeCycleTest/app/src/main/java/net/developerly/project03_activitylifecycletest/NormalActivity.java
@@ -0,0 +1,13 @@
+package net.developerly.project03_activitylifecycletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.normal_layout);
+    }
+}
